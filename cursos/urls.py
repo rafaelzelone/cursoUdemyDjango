34d6fa56bf074ce465f0d6ee9bdd0d12e7515fdb
rafaelzelone <82285284+rafaelzelone@users.noreply.github.com>
2023-05-07 21:31:02 -0300

    Add files via upload

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CursoApiView, AvaliacaoApiView
+from .views import CursoApiView, AvaliacaoApiView, AvaliacoesApiView, CursosApiView
 
 
 urlpatterns = [
-    path('cursos/', CursoApiView.as_view(), name="cursos"),
-    path('avaliacoes/', AvaliacaoApiView.as_view(), name="avaliacao"),
+    path('cursos/', CursosApiView.as_view(), name="cursos"),
+    path('avaliacoes/', AvaliacoesApiView.as_view(), name="avaliacoes"),
+    path('cursos/<int:pk>/', CursoApiView.as_view(), name="curso"),
+    path('avaliacao/<int:pk>/', AvaliacaoApiView.as_view(), name="avaliacao"),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,35 +1,23 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
-from rest_framework import status
 
-class CursoApiView(APIView):
-
-    def get(self, request):
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
-    
-
-    def post(self, request):
-        serializer = CursoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+class CursosApiView(generics.ListCreateAPIView):
+    queryset= Curso.objects.all()
+    serializer_class = CursoSerializer
 
 
-class AvaliacaoApiView(APIView):
+class CursoApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset= Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def get(self, request):
-        avaliacoes = Curso.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
-    
 
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class AvaliacoesApiView(generics.ListAPIView):
+    queryset= Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+
+class AvaliacaoApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset= Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
